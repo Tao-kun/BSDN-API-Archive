a84fdfe585eebbe6f48af18784f614a36e37aab0
Yitao Zhuo <Tao-kun@users.noreply.github.com>
2019-08-30 11:32:46 +0800

    Remove resource file migrations

--- Migrations/20190830033216_RemoveResourceFile.Designer.cs
+++ Migrations/20190830033216_RemoveResourceFile.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BSDN_API.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BSDN_API.Migrations
 {
     [DbContext(typeof(BSDNContext))]
-    partial class BSDNContextModelSnapshot : ModelSnapshot
+    [Migration("20190830033216_RemoveResourceFile")]
+    partial class RemoveResourceFile
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
